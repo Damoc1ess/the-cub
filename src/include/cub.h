d099e2459a835cb300d718257f5e9f28982bbc42
Florent Flamion <fflamion@paul-f4Ar5s7.clusters.42paris.fr>
2025-01-22 22:37:01 +0100

    plus que les leaks et le tour est jouer

--- src/include/cub.h
+++ src/include/cub.h
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fflamion <fflamion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 22:30:07 by fflamion          #+#    #+#             */
+/*   Updated: 2025/01/22 22:30:08 by fflamion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_h
 # define CUB3D_h
 
-// # define BUFFER_SIZE 4028
 # define NORTH "./textures/north.xpm"
 # define SOUTH "./textures/south.xpm"
 # define WEST "./textures/west.xpm"
